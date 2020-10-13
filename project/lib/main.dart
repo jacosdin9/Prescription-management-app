adebc190c41a66c2940cab82b1469e25d8570304
Jacob Osdin <2314964o@student.gla.ac.uk>
2020-10-13 22:36:34 +0100

    initial commit

---
+++ project/lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:project/mainArea.dart';
+
+void main() {
+  runApp(MainArea());
+}
---
+++ project/lib/mainArea.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class MainArea extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text(
+        "MAIN AREA",
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
