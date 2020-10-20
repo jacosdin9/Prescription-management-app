1d424ea47f5b0f194aa5e2fd0bd9185784cda108
Jacob Osdin <2314964o@student.gla.ac.uk>
2020-10-20 17:01:53 +0100

    minor updates to yaml

--- project/lib/calendar_files/calendar.dart
+++ project/lib/calendar_files/calendar.dart
@@ -1,9 +1,31 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
 
-class CalendarPage extends StatelessWidget {
+class CalendarPage extends StatefulWidget {
   @override
-  Widget build(BuildContext context) {
-    return Text("CALENDAR");
+  _CalendarPageState createState() => _CalendarPageState();
+}
+
+class _CalendarPageState extends State<CalendarPage>{
+
+  //CalendarController _controller;
+  @override
+  void initState() {
+    super.initState();
+    //_controller = CalendarController();
   }
 
+  @override
+  void dispose() {
+    //_controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
 }
--- project/lib/dashboard_files/addPrescription.dart
+++ project/lib/dashboard_files/addPrescription.dart
@@ -35,116 +35,287 @@ class _AddPrescriptionState extends State<AddPrescription>{
 
     return MaterialApp(
       title: "Add Prescription",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Add Prescription"),
-        ),
-        body: Form(
-          key: _formKey,
-          child: Column(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          // floatingActionButton: Stack(
+          //   children: <Widget>[
+          //
+          //     //CANCEL BUTTON
+          //     Positioned(
+          //       left: 20,
+          //       bottom: 20,
+          //       child: FloatingActionButton(
+          //         backgroundColor: Colors.red,
+          //         child: Icon(Icons.cancel),
+          //         onPressed: () {
+          //           Navigator.pop(context);
+          //         },
+          //       ),
+          //     ),
+          //
+          //     //DONE BUTTON
+          //     Positioned(
+          //       right: 20,
+          //       bottom: 20,
+          //       child: FloatingActionButton(
+          //         backgroundColor: Colors.green,
+          //         child: Icon(Icons.done),
+          //         onPressed: () {
+          //           // Validate returns true if the form is valid, otherwise false.
+          //           if (_formKey.currentState.validate()) {
+          //             _formKey.currentState.save();
+          //             print(pName);
+          //             print(pDosage);
+          //             print(pMeasurement);
+          //             print(pNoOfReminders);
+          //             print(pStock);
+          //             FirebasePage().addPrescription(pName, pDosage, pMeasurement, pNoOfReminders, pStock);
+          //             Navigator.pop(context);
+          //           }
+          //         },
+          //       ),
+          //     ),
+          //   ],
+          // ),
+
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(
+                  icon: Icon(Icons.assignment_turned_in),
+                  child: Text("Required"),
+                ),
+                Tab(
+                  icon: Icon(Icons.access_alarms),
+                  child: Text("Optional"),
+                ),
+                Tab(
+                  icon: Icon(Icons.directions_bike),
+                  child: Text("Optional"),
+                ),
+              ],
+            ),
+            title: Text('Add Prescription'),
+          ),
+          body: Stack(
             children: <Widget>[
-              //PRESCRIPTION NAME
-              TextFormField(
-                onSaved: (String value){pName=value;},
-                focusNode: myFocusNode,
-                decoration: InputDecoration(
-                  labelText: "Prescription name",
-                  hintText: "Enter prescription name"
-                ),
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Please enter some text";
-                  }
-                  return null;
-                },
+              //CURRENT TAB PAGE VIEW
+              TabBarView(
+                children: [
+                  //PAGE1
+                  Scaffold(
+                    body: Form(
+                      key: _formKey,
+                      child: Column(
+                        children: <Widget>[
+
+                          //PRESCRIPTION NAME
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: new BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 15, right: 15, top: 5),
+                                child: TextFormField(
+                                  onSaved: (String value){pName=value;},
+                                  focusNode: myFocusNode,
+                                  decoration: InputDecoration(
+                                    labelText: "Prescription name",
+                                    hintText: "Enter prescription name",
+                                    border: InputBorder.none,
+                                  ),
+                                  validator: (value) {
+                                    if(value.isEmpty){
+                                      return "Please enter some text";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          //DOSAGE
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: new BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 15, right: 15, top: 5),
+                                child: TextFormField(
+                                  onSaved: (String value){pDosage=double.parse(value);},
+                                  focusNode: myFocusNode,
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    labelText: "Dosage",
+                                    hintText: "Enter required dosage",
+                                    border: InputBorder.none,
+                                  ),
+                                  validator: (value) {
+                                    if(value.isEmpty){
+                                      return "Please enter a value";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          //MEASUREMENT
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: new BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 15, right: 15, top: 5),
+                                child: TextFormField(
+                                  onSaved: (String value){pMeasurement=value;},
+                                  focusNode: myFocusNode,
+                                  decoration: InputDecoration(
+                                    labelText: "Measurement",
+                                    hintText: "Enter measurement",
+                                    border: InputBorder.none,
+                                  ),
+                                  validator: (value) {
+                                    if(value.isEmpty){
+                                      return "Please enter a valid measurement";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          //NO OF REMINDERS
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: new BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 15, right: 15, top: 5),
+                                child: TextFormField(
+                                  onSaved: (String value){pNoOfReminders=int.parse(value);},
+                                  focusNode: myFocusNode,
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    labelText: "No. of reminders",
+                                    hintText: "Enter required no. of reminders",
+                                    border: InputBorder.none,
+                                  ),
+                                  validator: (value) {
+                                    if(value.isEmpty){
+                                      return "Please enter a value";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+
+                          //STOCK
+                          Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Container(
+                              decoration: BoxDecoration(
+                                color: Colors.grey,
+                                borderRadius: new BorderRadius.circular(10.0),
+                              ),
+                              child: Padding(
+                                padding: EdgeInsets.only(left: 15, right: 15, top: 5),
+                                child: TextFormField(
+                                  onSaved: (String value){pStock=double.parse(value);},
+                                  focusNode: myFocusNode,
+                                  keyboardType: TextInputType.number,
+                                  decoration: InputDecoration(
+                                    labelText: "Stock",
+                                    hintText: "Enter stock number",
+                                    border: InputBorder.none,
+                                  ),
+                                  validator: (value) {
+                                    if(value.isEmpty){
+                                      return "Please enter a value";
+                                    }
+                                    return null;
+                                  },
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+
+                  //PAGE2
+                  Icon(Icons.directions_transit),
+
+                  //PAGE 3
+                  Icon(Icons.directions_bike),
+                ],
               ),
 
-              //DOSAGE
-              TextFormField(
-                onSaved: (String value){pDosage=double.parse(value);},
-                focusNode: myFocusNode,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Dosage",
-                  hintText: "Enter required dosage",
-                ),
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Please enter a value";
-                  }
-                  return null;
-                },
-              ),
-
-              //MEASUREMENT
-              TextFormField(
-                onSaved: (String value){pMeasurement=value;},
-                focusNode: myFocusNode,
-                decoration: InputDecoration(
-                  labelText: "Measurement",
-                  hintText: "Enter measurement",
-                ),
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Please enter a valid measurement";
-                  }
-                  return null;
-                },
-              ),
-
-              //NO OF REMINDERS
-              TextFormField(
-                onSaved: (String value){pNoOfReminders=int.parse(value);},
-                focusNode: myFocusNode,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "No. of reminders",
-                  hintText: "Enter required no. of reminders",
-                ),
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Please enter a value";
-                  }
-                  return null;
-                },
-              ),
-
-              //STOCK
-              TextFormField(
-                onSaved: (String value){pStock=double.parse(value);},
-                focusNode: myFocusNode,
-                keyboardType: TextInputType.number,
-                decoration: InputDecoration(
-                  labelText: "Stock",
-                  hintText: "Enter stock number",
-                ),
-                validator: (value) {
-                  if(value.isEmpty){
-                    return "Please enter a value";
-                  }
-                  return null;
-                },
-              ),
-              RaisedButton(
-                onPressed: () {
-                  // Validate returns true if the form is valid, otherwise false.
-                  if (_formKey.currentState.validate()) {
-                    _formKey.currentState.save();
-                    print(pName);
-                    print(pDosage);
-                    print(pMeasurement);
-                    print(pNoOfReminders);
-                    print(pStock);
-                    FirebasePage().addPrescription(pName, pDosage, pMeasurement, pNoOfReminders, pStock);
+              //CANCEL BUTTON
+              Positioned(
+                left: 20,
+                bottom: 20,
+                child: FloatingActionButton(
+                  heroTag: "FAB1",
+                  backgroundColor: Colors.red,
+                  child: Icon(Icons.cancel),
+                  onPressed: () {
                     Navigator.pop(context);
-                  }
-                },
-                child: Text('Submit'),
-              )
+                  },
+                ),
+              ),
+
+              //DONE BUTTON
+              Positioned(
+                right: 20,
+                bottom: 20,
+                child: FloatingActionButton(
+                  heroTag: "FAB2",
+                  backgroundColor: Colors.green,
+                  child: Icon(Icons.done),
+                  onPressed: () {
+                    // Validate returns true if the form is valid, otherwise false.
+                    if (_formKey.currentState.validate()) {
+                      _formKey.currentState.save();
+                      print(pName);
+                      print(pDosage);
+                      print(pMeasurement);
+                      print(pNoOfReminders);
+                      print(pStock);
+                      FirebasePage().addPrescription(pName, pDosage, pMeasurement, pNoOfReminders, pStock);
+                      Navigator.pop(context);
+                    }
+                  },
+                ),
+              ),
+
             ],
           ),
+
+
         ),
       ),
+
+
     );
 
 
