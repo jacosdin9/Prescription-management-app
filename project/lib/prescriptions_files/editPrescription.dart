d7712c9098b43186ff67f10484d4b925c5ded15e
Jacob Osdin <2314964o@student.gla.ac.uk>
2021-01-26 14:28:24 +0000

    edit prescription progress

--- project/lib/prescriptions_files/editPrescription.dart
+++ project/lib/prescriptions_files/editPrescription.dart
@@ -41,27 +41,187 @@ class _EditPrescriptionState extends State<EditPrescription> {
                       sliver: SliverList(
                         delegate: SliverChildListDelegate(
                           [
-                            Container(
+                            //NAME
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
                                 padding: const EdgeInsets.symmetric(
                                     vertical: 25.0),
                                 height: 70,
                                 decoration: BoxDecoration(
                                     border: Border.all(color: Colors.blueAccent)
                                 ),
-                                child: Text("ID: " + data.id),
+                                child: Text("Name: " + data.name),
+                              ),
                             ),
 
                             SizedBox(height: 5),
 
-                            Container(
-                              padding: const EdgeInsets.symmetric(
-                                  vertical: 25.0),
-                              height: 70,
-                              decoration: BoxDecoration(
-                                  border: Border.all(color: Colors.blueAccent)
+                            //STRENGTH
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Strength: " + data.strength.toString()),
                               ),
-                              child: Text("Name: " + data.name),
                             ),
+
+                            SizedBox(height: 5),
+
+                            //STRENGTH UNITS
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Strength Units: " + data.strengthUnits),
+                              ),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //UNITS PER DOSAGE
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Units Per Dosage: " + data.unitsPerDosage.toString()),
+                              ),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //STOCK REMINDERS
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Stock Reminders: "),
+                              ),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //REMAINING STOCK
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Remaining Stock: " + data.stockNo.toString()),
+                              ),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //REMINDER FREQUENCY
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Reminder Frequency: " + data.reminderFreq),
+                              ),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //REMINDER DAYS
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: data.reminderFreq == "Specific days" ?
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Reminder Days"),
+                              ) : SizedBox(),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //DAYS INTERVAL
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: data.reminderFreq == "Days Interval" ?
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Name: " + data.daysInterval.toString()),
+                              ) : SizedBox(height: 0),
+                            ),
+
+                            SizedBox(height: 5),
+
+                            //REMINDER TIMES
+                            GestureDetector(
+                              onTap: () {
+
+                              },
+                              child: data.reminderFreq != "None" ?
+                              Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 25.0),
+                                height: 70,
+                                decoration: BoxDecoration(
+                                    border: Border.all(color: Colors.blueAccent)
+                                ),
+                                child: Text("Reminder Times"),
+                              ) :SizedBox(height: 0),
+                            ),
+
                           ],
                         ),
                       ),
